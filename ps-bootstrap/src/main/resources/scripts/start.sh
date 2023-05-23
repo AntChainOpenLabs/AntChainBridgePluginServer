52cf4d26e17a504ed29e89a428af7374633fd6fd
zouxyan <zouxueyan.zxy@antgroup.com>
2023-05-23 20:09:58 +0800

    [doc] [*] [v0.2.0] update version to 0.2.0 in pom.xml and README.md

--- ps-bootstrap/src/main/resources/scripts/start.sh
+++ ps-bootstrap/src/main/resources/scripts/start.sh
@@ -7,7 +7,9 @@ print_title
 
 log_info "start plugin-server now..."
 
-java -jar -Dlogging.file.path=${CURR_DIR}/../log ${CURR_DIR}/../lib/ps-bootstrap-0.1.2-SNAPSHOT.jar --spring.config.location=file:${CURR_DIR}/../config/application.yml > /dev/null 2>&1 &
+JAR_FILE=`ls ${CURR_DIR}/../lib/ | grep '.jar'`
+
+java -jar -Dlogging.file.path=${CURR_DIR}/../log ${CURR_DIR}/../lib/${JAR_FILE} --spring.config.location=file:${CURR_DIR}/../config/application.yml > /dev/null 2>&1 &
 if [ $? -ne 0 ]; then
     log_error "failed to start plugin-server"
     exit 1
